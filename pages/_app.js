7d74dcebafae4640a397d916eed1c8e6caddd114
RealSGII2 <rbxsgii2@gmail.com>
2020-08-04 14:58:31 -0700

    test fix 1

--- pages/_app.js
+++ pages/_app.js
@@ -28,40 +28,7 @@ function MyApp({ Component, pageProps }) {
 
 	return (
 		<div>
-			<Appbar title="r" showMenu onMenuClick={() => setOpen(!isOpen)} />
-			<Layout drawerOpen={isOpen} onClose={() => setOpen(false)} simple>
-				<Layout.Drawer>
-					<List look="rounded">
-						{Nav.map(n => {
-							if (n.type == "divider") return (
-								<Divider key={n} />
-							);
-							if (n.type == "label") return (
-								<List.Label key={n}>
-									{n.text}
-								</List.Label>
-							);
-							if (n.type == "ext-link") return (
-								<a href={n.to} key={n}>
-									<List.Item>
-										{n.text}
-									</List.Item>
-								</a>
-							);
-							return (
-								<Link key={n} href={n.to} activeClassName="active">
-									<List.Item>
-										{n.text}
-									</List.Item>
-								</Link>
-							)
-						})}
-					</List>
-				</Layout.Drawer>
-				<Layout.Content>
-					<Component {...pageProps} />
-				</Layout.Content>
-			</Layout>
+			<Component {...pageProps} />
 		</div>
   	)
 }
