bd899f6f254040a7a2253055c03106da2bb95371
RealSGII2 <rbxsgii2@gmail.com>
2020-08-05 14:56:50 -0700

    Add 1 page :p

--- components/Article.js
+++ components/Article.js
@@ -1,11 +1,28 @@
 import Head from 'next/head'
 import { Typography, ButtonToolbar, Button, Card, Badge } from 'rsgui'
 import { Grid, Row, Col } from 'react-flexbox-grid/dist/react-flexbox-grid';
+import { NextSeo } from 'next-seo';
 import styles from '../styles/Home.module.css'
 
 export default function BlogPost(props) {
 	return (
-		<>	
+		<>
+			<NextSeo
+				title={props.title}
+				description={props.description}
+				openGraph={{
+					type: "website",
+					title: props.title,
+					description: props.description,
+					site_name: "RealSGII2"
+				}}
+				additionalMetaTags={[
+					{
+						property: 'theme-color',
+						content: '#5093ad'
+					}
+				]}
+			/>	
 			<div className={styles.article}>
 				<div className={styles["a-head"]}>
 					<Typography type="h3">
--- pages/posts/dont-ask-to-ask.js
+++ pages/posts/dont-ask-to-ask.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { Typography } from 'rsgui'
 import { Grid, Row, Col } from 'react-flexbox-grid/dist/react-flexbox-grid';
-import { NextSeo } from 'next-seo';
 import Article from '../../components/Article'
 import styles from '../../styles/Home.module.css'
 
@@ -21,29 +20,37 @@ const description = "The worst possible thing you can do is ask to ask. Why not 
 export default function BlogPost() {
 	return (
 		<>
-			<NextSeo
-				title={title}
-				description={description}
-				openGraph={{
-					type: "website",
-					title,
-					description,
-					site_name: "RealSGII2"
-				}}
-				additionalMetaTags={[
-					{
-						property: 'theme-color',
-						content: '#5093ad'
-					}
-				]}
-			/>
 			<Article
 				title={title}
 				description={description}
 			>
-				<Typography type="body1">
-					Hi
-				</Typography>
+				<p>
+					Consider this statement:
+				</p>
+				<pre>
+					User1: i need help
+				</pre>
+				<p>
+					You're stating you need help, but not your question. The other person can't help you if
+					they don't know the problem. Most supporters on Discord take a look at the support channel every now and then, but
+					if you don't state your problem they're most likely going to skip over your message and not respond to you. <br /><br />
+
+					Instead, ask this:
+				</p>
+				<pre>
+					User1: hello, ive... [question]
+				</pre>
+				<p>
+					If you do this, you're not wasting time but you'll also get your response sooner.
+				</p>
+				<pre>
+					User2: So... [answer]
+				</pre>
+				<p>
+					A plus to asking your question in the same message is good for direct messaging. Let's say you state you have a problem 
+					but not your question. If you leave before the other user comes back, they have to wait until <i>you</i> come back. <br />
+					If you ask your question in the same message, the other user will be able to easily reply to you without waiting even a second.
+				</p>
 			</Article>
 		</>
 	)
